70b749beec84c82b87410a2377c9b04a2f079521
Yordan Veselinov <145705867+YordanVeselinov@users.noreply.github.com>
2023-11-13 00:15:52 +0200

    Delete First Steps in Coding - Exercise directory

--- First Steps in Coding - Exercise/vacation_books_list.py
+++
@@ -1,8 +0,0 @@
-import math
-pages = int(input())
-pages_per_hour = int(input())
-days = int(input())
-one_hour = pages / pages_per_hour
-needed_hours_per_day = one_hour / days
-
-print(math.floor(needed_hours_per_day))

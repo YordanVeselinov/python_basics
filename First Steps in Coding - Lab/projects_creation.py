9782c7e8d53d51fad5c2d08b09099767461e2428
Yordan Veselinov <145705867+YordanVeselinov@users.noreply.github.com>
2023-11-13 00:16:02 +0200

    Delete First Steps in Coding - Lab directory

--- First Steps in Coding - Lab/projects_creation.py
+++
@@ -1,7 +0,0 @@
-name = input()
-projects = int(input())
-
-hours_needed = projects * 3
-
-print(f"The architect {name} will need {hours_needed}"
-      f" hours to complete {projects} project/s.")
--- First Steps in Coding - Lab/yard_reening.py
+++
@@ -1,7 +0,0 @@
-area = float(input())
-
-price_before_discount = area * 7.61
-discount = price_before_discount * 0.18
-real_price = price_before_discount - discount
-print(f"The final price is: {real_price} lv.")
-print(f"The discount is: {discount} lv.")
